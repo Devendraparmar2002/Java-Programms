73e046ca9520ced7066ea1d955dc17a1f058266d
Devendra Parmar <99470456+Devendraparmar2002@users.noreply.github.com>
2022-02-14 08:58:20 +0530

    Add files via upload

---
+++ Addtwonumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Addtwonumbers
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int FirstNumber = sc.nextInt();
+		int SecondNumber = sc.nextInt();
+		int result = FirstNumber + SecondNumber;
+		System.out.print(result);
+	}
+}
+
+
+
+
+/*output:
+15
+10
+35
+*/
